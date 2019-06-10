22e2cdd5dea50792a6c3ade6bdf2fd03c93901d8
Katy Venables <katy_venables@yahoo.co.uk>
2019-06-10 04:43:50 +0100

    Initial commit

---
+++ common/defines/ci_defines.lua
@@ -0,0 +1,5 @@
+NDefines.NAI.AI_CONVERT_CULTURES = 0 					--AI uses this very stupidly
+NDefines.NCountry.CORE_LOSE_CULTURE_GROUP = -1 			--This leads to better gameplay with CI
+NDefines.NCountry.MONTHS_TO_CHANGE_CULTURE = 20 		--4x Increase
+NDefines.NCountry.PS_CHANGE_CULTURE = 20				--4x Increase
+NDefines.NCountry.ENFORCE_CULTURE_TAX_MULTIPLIER = 0.5	--2x Decrease
