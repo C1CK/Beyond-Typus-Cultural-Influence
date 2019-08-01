8384d7b5e28f85f42c5c90644d0bf0f4fc73e6a8
Katy Venables <katy_venables@yahoo.co.uk>
2019-08-01 17:13:04 +0100

    Steppe improvement changes to razeing

--- common/defines/ci_defines.lua
+++ common/defines/ci_defines.lua
@@ -5,3 +5,15 @@ NDefines.NCountry.PS_CHANGE_CULTURE = 1000				--Disabled
 NDefines.NCountry.ENFORCE_CULTURE_TAX_MULTIPLIER = 0.5	--2x Decrease
 NDefines.NReligion.EXPEL_RELIGIOUS_MINORITY_THRESHOLD = 100
 NDefines.NAI.CHARTER_COMPANY_BASE_RELUCTANCE = -10000	-- Disabled
+
+	--Steppe Improvements (To be splintered into separate mod later)
+NDefines.NCountry.RAZE_UNREST_DURATION = 10							--Same
+NDefines.NCountry.RAZE_PROVINCE_DEVELOPMENT_DECREASE = 0.33			--Same
+NDefines.NCountry.RAZE_PROVINCE_POWER_PER_DEVELOPMENT = 10			--From 25p0
+NDefines.NCountry.RAZE_TECH_BASE_LEVEL = 12							--3 -> 12 (1557)
+NDefines.NCountry.RAZE_TECH_POWER_DECREASE = 0.02					--0p04 -> 0p02
+NDefines.NCountry.RAZE_TECH_POWER_DECREASE_MAX = 0.4				--0p02 -> 0p04 (Want to be harsher but have a reduced gradient)
+NDefines.NCountry.RAZE_PROVINCE_COOLDOWN_YRS = 20					--Same
+NDefines.NCountry.RAZE_PROVINCE_CONQUERED_TIME_LIMIT_MONTHS = 6		--Same
+NDefines.NCountry.HORDE_UNITY_PER_LOOT = 0.25						--Same
+NDefines.NCountry.HORDE_UNITY_PER_RAZE = 0.25						--0p5 -> 0p25
