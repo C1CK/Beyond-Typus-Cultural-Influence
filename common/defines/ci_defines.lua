30ddee73f9f7bb6bb896b16c329515ce999203c3
Katy Venables <katy_venables@yahoo.co.uk>
2019-08-03 02:45:19 +0100

    Reenabled culture conversion, as new way of disabling expel minorities was found

--- common/defines/ci_defines.lua
+++ common/defines/ci_defines.lua
@@ -1,10 +1,9 @@
 NDefines.NAI.AI_CONVERT_CULTURES = 0 					--AI uses this very stupidly
 NDefines.NCountry.CORE_LOSE_CULTURE_GROUP = -1 			--This leads to better gameplay with CI
-NDefines.NCountry.MONTHS_TO_CHANGE_CULTURE = 1000 		--Disabled
-NDefines.NCountry.PS_CHANGE_CULTURE = 1000				--Disabled
-NDefines.NCountry.ENFORCE_CULTURE_TAX_MULTIPLIER = 0.5	--2x Decrease
-NDefines.NReligion.EXPEL_RELIGIOUS_MINORITY_THRESHOLD = 100
-NDefines.NAI.CHARTER_COMPANY_BASE_RELUCTANCE = -10000	-- Disabled
+NDefines.NCountry.MONTHS_TO_CHANGE_CULTURE = 20 		--4x Increase
+NDefines.NCountry.PS_CHANGE_CULTURE = 20				--4x Increase
+NDefines.NCountry.ENFORCE_CULTURE_TAX_MULTIPLIER = 0.5	--2x Decrease 
+NDefines.NAI.CHARTER_COMPANY_BASE_RELUCTANCE = -10000	--Disabled
 
 	--Steppe Improvements (To be splintered into separate mod later)
 NDefines.NCountry.RAZE_UNREST_DURATION = 10							--Same
